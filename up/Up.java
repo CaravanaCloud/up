4c3f16838bd313ed4fcb50701a19607231b3ce0b
Julio Faerman <jufaerma@redhat.com>
2023-04-18 18:39:30 +0200

    wip

---
+++ up/Up.java
@@ -0,0 +1,12 @@
+package up;
+
+import io.quarkus.runtime.QuarkusApplication;
+
+public class Up implements QuarkusApplication {
+    
+    @Override
+    public int run(String... args){
+        System.out.println("Starting up cli, thank you for running <3");
+    }
+
+}
