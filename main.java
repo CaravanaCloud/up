0786471ab167de2a8808be513d69871edacd9a1b
Julio Faerman <jufaerma@redhat.com>
2023-04-18 18:41:14 +0200

    wip

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS io.quarkus.platform:quarkus-bom:2.16.6.Final@pom
-//DEPS io.quarkus.platform:quarkus-arc:2.16.6.Final
-//DEPS io.quarkus.platform:quarkus-picocli:2.16.6.Final
+//DEPS io.quarkus:quarkus-arc:2.16.6.Final
+//DEPS io.quarkus:quarkus-picocli:2.16.6.Final
 
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
