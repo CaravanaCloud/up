af83b8f1fc2ee4cd872f90c8932be4a00400bd34
Julio Faerman <jufaerma@redhat.com>
2023-05-17 13:21:02 +0200

    removing dead code

--- archive/scripts/aws-nuke/main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# assert: aws sts get-caller-identity
-# install: aws-nuke
-# template: aws-nuke.qute.yaml
-# log: aws-nuke.log
-echo aws-nuke -c ./aws-nuke.yaml \
-  --no-dry-run \
-  --force \
-  --force-sleep 3
