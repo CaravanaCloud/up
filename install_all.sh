02f8c776db1802b4d48f77bc901030d9d096d421
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-06-23 15:07:48 +0000

    wip[

---
+++ install_all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+set -ex
+
+pushd $DIR
+poetry env use 3.11
+poetry cache clear . --all --no-interaction
+
+echo "Checking environment"
+echo "pyenv version \n $(pyenv version)"
+echo "poetry env info \n $(poetry env info)"
+# sleep 333
+
+pushd "up_lib"
+echo "Installing up_lib"
+poetry build
+pip install "dist/"*.whl
+popd
+
+pushd "up_cli"
+echo "Installing up_cli"
+poetry build
+pip install "dist/"*.whl
+popd
+
+popd
+
+echo done
