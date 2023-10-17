657742d998d411b956d0d995ab3a8e2d07a18229
Julio Faerman <jufaerma@redhat.com>
2023-10-17 11:31:32 +0200

    wip

--- up_cli/src/up_cli/hookspec.py
+++ up_cli/src/up_cli/hookspec.py
@@ -6,11 +6,9 @@ hookspec = pluggy.HookspecMarker("up")
 @hookspec(firstresult=True)
 def image_for_prompt(prompt: list[str]) -> str:
     """Present the image in which the prompt will be executed"""
-    None
 
 @hookspec(firstresult=True)
 def substitutions_for_prompt(prompt: list[str]) -> list[list[str]]:
     """Present the substitutions for the prompt"""
-    None
     
 # TODO: Variables and Volumes
