30df9d0a7817a8bdfbb3ee97975bea11891d7728
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-10-27 15:49:04 +0000

    wip

--- up_ansible/up_ansible/__init__.py
+++ up_ansible/up_ansible/__init__.py
@@ -1,4 +1,4 @@
 from up_lib import pm
-from up_ansible import run_for_prompt
+from . import run_for_prompt
 
 pm.register(run_for_prompt)
--- up_lib/up_lib/hookspec.py
+++ up_lib/up_lib/hookspec.py
@@ -2,7 +2,6 @@ import pluggy
 from up_lib import Prompt
 from up_lib.containers import RunConfigs
 
-hookspec = pluggy.HookspecMarker("up")
 
 @hookspec(firstresult=True)
 def run_for_prompt(prompt: Prompt) -> RunConfigs:
