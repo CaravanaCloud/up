479d0ef6fb0f597ba553c0842199833321611f94
Julio Faerman <356476+faermanj@users.noreply.github.com>
2023-11-17 11:38:34 +0000

    Unified Settings class

--- uplib/uplib/config.py
+++ uplib/uplib/config.py
@@ -5,40 +5,79 @@ from dynaconf import Dynaconf
 from dynaconf.loaders import yaml_loader
 from enum import Enum
 
-class Config(Enum):
-    # (key, default_value)
-    default_image = ("default_image", "fedora")
-    welcome_message = ("welcome_message", "Thank your for running UP!")
-    log_level = ("log_level", "INFO", logging.getLevelName)
-    volumes = ("volumes", {})
-    ports = ("ports", {})
-
-    __settings = None
-
-    #TODO: Add optional prompt
+class Settings:
+    _settings = {}
+    
+    settings = None
+    
     @classmethod
-    def settings(cls):
-        _settings = Config.__settings
-        if not _settings:
-            _settings = Dynaconf(
+    def of_default_settings(cls):
+        settings = Settings._settings.get("__DEFAULT__")
+        if not settings:
+            logging.info("Loading default settings")
+            settings = Dynaconf(
                     environments=False,
                     envvar_prefix="UP")
             up_yaml = pkgutil.get_data(__name__, "up.yaml")
             if up_yaml:
                 up_yaml = up_yaml.decode("utf-8")
-                yaml_loader.load(_settings, filename=up_yaml)
-        return _settings
+                yaml_loader.load(settings, filename=up_yaml)
+            Settings._settings["__DEFAULT__"] = settings
+        return settings
 
-    def get(self):
-        value = self.value
-        key = value[0]
-        result = Config.settings().get(key)
-        if not result:
-            result = value[1] if len(value) > 1 else None
-        converter = value[2] if len(value) > 2 else None
-        if converter and result:
-            result = converter(result)
+    @classmethod
+    def prefix_for(cls, plugin_mod):
+        plugin_name = plugin_mod.__name__
+        plugin_name = plugin_name.replace("-", "_")
+        plugin_name = plugin_name.upper()
+        return plugin_name
+
+    @classmethod
+    def of_plugin_settings(cls, plugin_name, settings_path):
+        settings = Settings._settings.get(plugin_name)
+        if not settings:
+            env_prefix = Settings.prefix_for(plugin_name)
+            settings = Dynaconf(
+                load_dotenv=True,
+                envvar_prefix=env_prefix,
+                settings_file=settings_path,)
+            Settings._settings[plugin_name] = settings
+        return settings
+    
+    def __init__(self, plugin_name = None, settings_path = None):
+        if plugin_name:
+            self.settings = Settings.of_plugin_settings(plugin_name, settings_path)
+        else:
+            self.settings = Settings.of_default_settings()
+    
+    def settings(self) -> Dynaconf:
+        return self.settings
+    
+    def get(self, config_enum):
+        config = config_enum.value
+        default_val = config[1] if len(config) > 1 else None
+        config_key = config[0]
+        result = self._settings.get(config_key, default_val)
+        if result:  
+            converter = config[2] if len(config) > 2 else None
+            if converter:
+                result = converter(result)
         return result
+
+class Config(Enum):
+    # (key, default_value)
+    default_image = ("default_image", "fedora")
+    welcome_message = ("welcome_message", "Thank your for running UP!")
+    log_level = ("log_level", "INFO")
+    volumes = ("volumes", {})
+    ports = ("ports", {})
+    prompts = ("prompts", [])
+    
+    # Gets a configuration value from default settings
+    def get(self):
+        result = Settings().get(self)
+        return result
+
 
 def get_log_level():
     return Config.log_level.get()
--- uplib/uplib/logging.py
+++ uplib/uplib/logging.py
@@ -1,14 +1,26 @@
 import sys
 import logging
 from .config import get_log_level
+from rich.logging import RichHandler
 
 TRACE = 5
 
 def init_logging():
     sys.set_int_max_str_digits(999999)    
     level = get_log_level()
-    logging.basicConfig(stream=sys.stderr, level=level)
+    print("Initializing logging with level "+str(level))
+    logging.basicConfig(
+        level=level
+    )
+    #TODO: Configure rich log handler
+    # https://rich.readthedocs.io/en/stable/logging.html
     log = logging.getLogger("up")
+    log.setLevel(level)
+    # print("Testing logging PRINT at level "+str(level))
+    # log.info("Testing logging INFO at level "+str(level))
+    # log.error("Testing logging ERROR at level "+str(level))
+    # log.debug("Testing logging DEBUG at level "+str(level))
+    # log.warning("Testing logging WARNING at level "+str(level))
     return log
 
 log = init_logging()
